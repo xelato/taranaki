9d04e59c7b8d1b7d0e0c2285fe26368f2155c107
🚀 niko <rocketniko@pm.me>
2026-03-20 15:02:05 +0200

    define PY.CALL

--- crates/taranaki-python/src/lib.rs
+++ crates/taranaki-python/src/lib.rs
@@ -10,15 +10,34 @@ use redis_module::redis_module;
 use redis_module::{Context, NextArg, RedisError, RedisResult, RedisString};
 
 /*
-PY.EVAL <EXPRESSION> [ARG [ARG [...]]]
-PY.EVAL_RO <EXPRESSION> [ARG [ARG [...]]]
-PY.EVAL_RX <EXPRESSION> [ARG [ARG [...]]]
-Evaluate a Python expression in RW, RO or RX mode.
-    RW - access to all available commands
+Modes:
+    RW - access to all available server commands (read-only/write)
     RO - access to read-only commands
     RX - restricted execution, no commands access
+
+PY.EVAL <code> [ARG [ARG [...]]]
+PY.EVAL_RO <code> [ARG [ARG [...]]]
+PY.EVAL_RX <code> [ARG [ARG [...]]]
+Evaluate a Python code snippet.
+
+PY.CALL <key> [ARG [ARG [...]]]
+PY.CALL_RO <key> [ARG [ARG [...]]]
+PY.CALL_RX <key> [ARG [ARG [...]]]
+Evaluate a Python code snippet stored at `key`.
+
+PY.HTTP <key> <method> <url> [HEADER <name>:<value> [HEADER <name>:<value> [...]]] [CONTENT <content>]
+
+127.0.0.1:6379> SET /app/hello "r=http_request(); http_response(200, text='Hello, ' + r.args['name'])"
+127.0.0.1:6379> PY.HTTP /app/hello GET /hello?name=Taranaki
 */
-fn python_eval(ctx: &Context, args: Vec<RedisString>, mode: Mode) -> RedisResult {
+
+fn call_or_eval(
+    ctx: &Context,
+    args: Vec<RedisString>,
+    mode: Mode,
+    is_call: bool,
+    is_http: bool,
+) -> RedisResult {
     let num_args = args.len();
     if num_args < 2 {
         return Err(RedisError::WrongArity);
@@ -29,8 +48,23 @@ fn python_eval(ctx: &Context, args: Vec<RedisString>, mode: Mode) -> RedisResult
     // command
     let _command = args_iter.next_str()?;
 
-    // python expression
-    let code: &str = args_iter.next_str()?;
+    // python code
+    let code: String = if is_call {
+        // key
+        let key: &str = args_iter.next_str()?;
+        // fetch code from key
+        let _code = ctx.call("GET", &[key])?;
+        let Some(value) = convert::rv_try_string(_code) else {
+            // todo: include key in error message
+            return Err(RedisError::String(String::from(format!(
+                "code not set or wrong type at key: {key}"
+            ))));
+        };
+        value
+    } else {
+        let _code = args_iter.next_str()?;
+        String::from(_code)
+    };
 
     // arguments
     let mut argv: Vec<String> = Vec::new();
@@ -46,79 +80,45 @@ fn python_eval(ctx: &Context, args: Vec<RedisString>, mode: Mode) -> RedisResult
     }
 
     let commander = commander::Commander::get_instance(ctx, mode.clone(), argv)?;
-    Ok(crate::eval::eval(&commander, code.to_owned(), mode.clone()))
+    // todo: lossless/lossy serialization
+    // todo: convert exceptions/errors and incorrect return types to 503
+    // todo: namedtuple support
+    // todo: implement http_request()
+    // todo: implement http_response()
+    Ok(crate::eval::eval(&commander, code, mode.clone()))
 }
 
 pub fn python_eval_rw(ctx: &Context, args: Vec<RedisString>) -> RedisResult {
-    python_eval(ctx, args, Mode::RW)
+    call_or_eval(ctx, args, Mode::RW, false, false)
 }
 
 pub fn python_eval_ro(ctx: &Context, args: Vec<RedisString>) -> RedisResult {
-    python_eval(ctx, args, Mode::RO)
+    call_or_eval(ctx, args, Mode::RO, false, false)
 }
 
 pub fn python_eval_rx(ctx: &Context, args: Vec<RedisString>) -> RedisResult {
-    python_eval(ctx, args, Mode::RX)
+    call_or_eval(ctx, args, Mode::RX, false, false)
 }
 
-/*
+pub fn python_call_rw(ctx: &Context, args: Vec<RedisString>) -> RedisResult {
+    call_or_eval(ctx, args, Mode::RW, true, false)
+}
 
-PY.HTTP <key> <method> <url> [HEADER <name>:<value> [HEADER <name>:<value> [...]]] [CONTENT <content>]
+pub fn python_call_ro(ctx: &Context, args: Vec<RedisString>) -> RedisResult {
+    call_or_eval(ctx, args, Mode::RO, true, false)
+}
 
-127.0.0.1:6379> SET /app/hello "r=http_request(); http_response(200, text='Hello, ' + r.args['name'])"
-127.0.0.1:6379> PY.HTTP /app/hello GET /hello?name=Taranaki
-*/
-fn python_http(ctx: &Context, args: Vec<RedisString>, mode: Mode) -> RedisResult {
-    let num_args = args.len();
-    if num_args < 4 {
-        return Err(RedisError::WrongArity);
-    }
-
-    let mut args_iter = args.into_iter();
-
-    // command
-    let _command = args_iter.next_str()?;
-
-    // key
-    let key: &str = args_iter.next_str()?;
-
-    // arguments
-    let mut argv: Vec<String> = Vec::new();
-    // first argument is script name, akin to cpython
-    argv.push(String::from(key));
-
-    // note: arguments are not interpreted (yet) as `HTTPRequest`.
-    // This happens later during call to `http_request()`.
-    // This allows to keep generality while not doing something twice on the ok path.
-    loop {
-        let Some(value) = args_iter.next() else {
-            break;
-        };
-        // arguments are expected to be encoded in utf-8
-        argv.push(value.to_string_lossy());
-    }
-
-    // fetch code from key
-    let _code = ctx.call("GET", &[key])?;
-    let Some(code) = convert::rv_try_string(_code) else {
-        return Err(RedisError::String(String::from("code not set or wrong type")));
-    };
-
-    let commander = commander::Commander::get_instance(ctx, mode.clone(), argv)?;
-    // todo: convert exceptions/errors and incorrect return types to 503
-    // todo: namedtuple support
-    Ok(crate::eval::eval(&commander, code.to_owned(), mode.clone()))
-
+pub fn python_call_rx(ctx: &Context, args: Vec<RedisString>) -> RedisResult {
+    call_or_eval(ctx, args, Mode::RX, true, false)
 }
 
 pub fn python_http_ro(ctx: &Context, args: Vec<RedisString>) -> RedisResult {
-    python_http(ctx, args, Mode::RO)
+    call_or_eval(ctx, args, Mode::RO, true, true)
 }
 
 pub fn python_http_rw(ctx: &Context, args: Vec<RedisString>) -> RedisResult {
-    python_http(ctx, args, Mode::RW)
+    call_or_eval(ctx, args, Mode::RW, true, true)
 }
-
 
 redis_module! {
     name: "taranaki-python",
@@ -129,6 +129,11 @@ redis_module! {
         ["py.eval", python_eval_rw, "", 0, 0, 0, ""],
         ["py.eval_ro", python_eval_ro, "", 0, 0, 0, ""],
         ["py.eval_rx", python_eval_rx, "", 0, 0, 0, ""],
+
+        ["py.call", python_call_rw, "", 0, 0, 0, ""],
+        ["py.call_ro", python_call_ro, "", 0, 0, 0, ""],
+        ["py.call_rx", python_call_rx, "", 0, 0, 0, ""],
+
         ["py.http", python_http_rw, "", 0, 0, 0, ""],
         ["py.http_ro", python_http_ro, "", 0, 0, 0, ""],
     ],
