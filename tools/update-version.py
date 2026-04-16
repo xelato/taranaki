4dc75e50e541bf69ceae0553addf352688df3df0
🚀 niko <rocketniko@pm.me>
2026-04-16 13:06:31 +0300

    update-version command

---
+++ tools/update-version.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+if len(sys.argv) != 2:
+    sys.exit("version required")
+
+version = sys.argv[1]
+if version.startswith("v"):
+    version = version[1:]
+
+print(version)
+
+root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+os.chdir(root)
+with open("VERSION", "w") as f:
+    f.write(version)
+
+with open("crates/taranaki-python/src/version.rs", "w") as f:
+    f.write(f"""pub static VERSION: &'static str = "{version}";\n""")
